f03673aeeb67f85e35a91e6986afcd2c8265aef1
seventeen <powdragon1@gmail.com>
2017-06-21 19:44:03 +0900

    ad block 기능 추가

---
+++ scripts/adBlock.js
@@ -0,0 +1,28 @@
+Element.prototype.remove = function() {
+    this.parentElement.removeChild(this);
+};
+
+NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
+    for (var i = this.length - 1; i >= 0; i--) {
+        if (this[i] && this[i].parentElement) {
+            this[i].parentElement.removeChild(this[i]);
+        }
+    }
+};
+
+function temp(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i])
+            arr[i].remove()
+    }
+}
+
+var adList = [
+    document.getElementsByClassName('_BTN_AD_'),
+    document.getElementsByClassName('adsByIdn32409'),
+    document.getElementsByClassName('daum_ddn_area'),
+    document.getElementById('list_right_ad'),
+    document.getElementById('wif_adx_banner_wrap')
+];
+
+temp(adList);
